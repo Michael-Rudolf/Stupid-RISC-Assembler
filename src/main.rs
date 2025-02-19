d062d719099f3c50db8366cd55567890a02f4474
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2025-02-19 18:04:07 +0100

    Added arguments support. for -v and -f.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,20 @@ use std::fs::File;
 use rfd::FileDialog;
 use std::io::prelude::*;
 use colored::Colorize;
+use std::env;
+use std::path::PathBuf;
 
 mod assembler;
 mod instruction;
 
 fn main() {
-    let path = FileDialog::new().pick_file().unwrap();
-
-    let display = path.display();
+    let args: Vec<String> = env::args().collect();
+    let mut input_path: PathBuf = Default::default();
+    get_inputs(args, &mut input_path);
+    let display = input_path.display();
 
     // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open(&path) {
+    let mut file = match File::open(&input_path) {
         Err(reason) => panic!("Couldn't open {}: {}", display, reason),
         Ok(file) => file,
     };
@@ -29,10 +32,55 @@ fn main() {
 
     let binary: Vec<u8> = assembler.output;
 
-    let store_path = path.to_str().unwrap().to_string().split('.').nth(0).unwrap().to_string() + ".bin";
-    if let binary_file = File::create(store_path){
-        _ = binary_file.unwrap().write_all(&binary);
-    }
+    let store_path = input_path.to_str().unwrap().to_string().split('.').nth(0).unwrap().to_string() + ".bin";
+    let binary_file = File::create(store_path);
+    _ = binary_file.unwrap().write_all(&binary);
+
 
     println!("{}", "Success!".to_string().green());
+}
+
+fn get_inputs(args: Vec<String>, input_path: &mut PathBuf) {
+    if args.contains(&"-v".to_string()){
+        // Visual setup
+        let dialog = FileDialog::new();
+        if let Some(path) = dialog.pick_file(){
+            *input_path = path;
+        }
+        return;
+    }
+    // Command line args setup
+    *input_path = PathBuf::from(get_parameter("-f", args.clone()));
+
+
+
+}
+
+fn get_parameter(name: &str, args: Vec<String>) -> String {
+    let mut parameter_position: Option<usize> = None;
+    for i in 0..args.len() - 1{
+        if let Some(argument) = args.get(i) {
+            if argument == name{
+                parameter_position = Some(i + 1);
+            }
+        }
+    }
+
+    if parameter_position.is_none() {
+        let error = format!("Parameter {} expected but not found", name).red().to_string();
+        panic!("{}", error);
+    }
+
+    if let Some(result) = args.iter().nth(parameter_position.unwrap()) { return result.to_string(); }
+    let error = format!("Parameter {} has an expected value.", name).red().to_string();
+    panic!("{}", error);
+}
+
+#[allow(dead_code)]
+fn get_parameter_uint(name: &str, args: Vec<String>) -> u64 {
+    let parameter_value = get_parameter(name, args);
+
+    if let Some(parameter_value_u64) = parameter_value.parse::<u64>().ok(){ return parameter_value_u64; }
+    let error = format!("Parameter {} should be a positive number but {} was found instead.", name, parameter_value).red().to_string();
+    panic!("{}", error);
 }
